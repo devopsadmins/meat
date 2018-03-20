98ad3cef571ef677d925ff5f4504b98c1f7f350a
webdevelopersjc <webdevelopersjc@yahoo.com.br>
2018-03-20 19:28:46 -0300

    first commit

---
+++ src/app/restaurant-detail/mt-reviews/mt-reviews.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-mt-reviews',
+  templateUrl: './mt-reviews.component.html'
+})
+export class MtReviewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/restaurants/restaurants.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Restaurant } from './restaurant/rastaurant.model';
+import { RestaurantsService } from './restaurants.service';
+
+@Component({
+  selector: 'mt-restaurants',
+  templateUrl: './restaurants.component.html'
+})
+export class RestaurantsComponent implements OnInit {
+
+restaurants: Restaurant[]
+
+  constructor(private restaurantesService:  RestaurantsService) { }
+
+  ngOnInit() {
+  this.restaurantesService.restaurants()
+    .subscribe(restaurants => this.restaurants = restaurants)
+  }
+
+}
