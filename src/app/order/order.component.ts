852806622a1dd3302811b1c29e7ab56bc7346390
webdevelopersjc <webdevelopersjc@yahoo.com.br>
2018-03-23 16:22:43 -0300

    Erro de order item

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-//import { NgForm } from '@angular/forms';
+import { RadioOption } from '../shared/radio/radio-option.model';
+import { OrderService } from './order.service';
+import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
 
 
 @Component({
@@ -8,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
-  constructor() { }
+paymentOptions: RadioOption[] = [
+  {label: 'Dinheiro', value: 'MON'},
+  {label: 'Cartão de Débito', value: 'DEB'},
+  {label: 'Cartão de Refeição', value: 'REF'}
+]
+
+  constructor(private orderService: OrderService) { }
 
   ngOnInit() {
+  }
+  cartItems() {
+    return  this.orderService.cartItems();
+  }
+
+  increaseQty(item: CartItem) {
+    return  this.orderService.increaseQty(item);
+  }
+  decreaseQty(item: CartItem) {
+    return  this.orderService.decreaseQty(item);
+  }
+  demove(item: CartItem) {
+    return  this.orderService.removeItem(item);
   }
 
 }
