6296a91a1e64029589c81f54a01a2a5bf6c59ad8
webdevelopersjc <webdevelopersjc@yahoo.com.br>
2018-03-26 17:46:39 -0300

    Modularizado With Providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,17 @@ import { HomeComponent } from './home/home.component';
 import { ROUTES } from './app.routes';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
-import { RestaurantsService } from './restaurants/restaurants.service';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { MtReviewsComponent } from './restaurant-detail/mt-reviews/mt-reviews.component';
 import { ReviewsComponent } from './reviews/reviews.component';
-import { LOCALE_ID } from '@angular/core';
+
 import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
-import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
-import { OrderService } from './order/order.service';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule } from './shared/shared.module';
+
 
 
 @NgModule({
@@ -36,7 +34,7 @@ import { SharedModule } from './shared/shared.module';
     MenuItemComponent,
     MtReviewsComponent,
     ReviewsComponent,
-        OrderSummaryComponent,
+    OrderSummaryComponent,
   ],
   imports: [
     BrowserModule,
@@ -44,12 +42,9 @@ import { SharedModule } from './shared/shared.module';
     RouterModule.forRoot(ROUTES),
     FormsModule,
     ReactiveFormsModule,
-    SharedModule
+    SharedModule.forRoot()
   ],
-  providers: [RestaurantsService,
-    { provide: LOCALE_ID, useValue: 'pt-BR'},
-    ShoppingCartService,
-    OrderService
+  providers: [
   ],
   bootstrap: [AppComponent]
 })
