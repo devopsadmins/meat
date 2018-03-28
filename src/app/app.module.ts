b5266ab776c9e2881a6736b088ef5c9e976e2471
webdevelopersjc <webdevelopersjc@yahoo.com.br>
2018-03-27 22:05:46 -0300

    Finalizado pra build - com HashStrategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { RouterModule, PreloadAllModules } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+
 import { ROUTES } from './app.routes';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
@@ -18,8 +19,9 @@ import { ReviewsComponent } from './reviews/reviews.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule } from './shared/shared.module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 
 @NgModule({
@@ -47,7 +49,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
     ReactiveFormsModule,
     SharedModule.forRoot()
   ],
-  providers: [
+  providers: [ { provide: LocationStrategy, useClass: HashLocationStrategy }
   ],
   bootstrap: [AppComponent]
 })
